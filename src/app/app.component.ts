620d2d731e55f11f474f9142484f62c6d0eec035
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2023-03-13 02:12:43 -0400

    Salida del resultado por pantalla y comunicacion con el backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {CurrencyService} from "./service/currency.service";
+import {ResponseCurrencyDto} from "./dto/response.currency.dto";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'software';
+  test = 'prueba';
+
+  currencyForm: FormGroup;
+  responseCurrencyDto: ResponseCurrencyDto;
+
+  constructor(private formBuilder: FormBuilder, private currencyService: CurrencyService) {
+    this.currencyForm = this.formBuilder.group({
+      from: '',
+      to: '',
+      amount: ''
+    })
+  }
+
+  submit() {
+    console.log(this.currencyForm.value);
+    this.currencyService.convertCurrency(this.currencyForm.value.from, this.currencyForm.value.to,
+      this.currencyForm.value.amount).subscribe({
+      next: (data) => {
+        console.log('Invocacion exitosa');
+        console.log(data);
+        this.responseCurrencyDto = data;
+        console.log('resultado');
+        console.log(this.responseCurrencyDto.result);
+      },
+      error: (error) => console.log("ERROR >>> ", error)
+    })
+    console.log('test');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
