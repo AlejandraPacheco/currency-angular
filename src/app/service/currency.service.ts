12b56054f173023e6fb5158bfdf204ba4642b2e6
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2023-03-22 12:09:33 -0400

    Docker-compose con nginx y proxy reverso funcionando

--- src/app/service/currency.service.ts
+++ src/app/service/currency.service.ts
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment.docker';
 })
 export class CurrencyService {
 
-  conversionesURL="http://localhost:8008/paginas?";
+  //conversionesURL="http://localhost:8008/paginas?";
   constructor(private http: HttpClient) {
   }
 
@@ -18,6 +18,6 @@ export class CurrencyService {
       + from + "&to=" + to + "&amount=" + amount);
   }
   public conversiones(page: number, size: number, sort: string, asc: boolean): Observable<any> {
-    return this.http.get<any>(this.conversionesURL+`page=${page}&size=${size}&sort=${sort}&asc=${asc}`);
+    return this.http.get<any>(`${environment.BACKEND_URL}/paginas?page=${page}&size=${size}&sort=${sort}&asc=${asc}`);
   }
 }
